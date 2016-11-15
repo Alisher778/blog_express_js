e09d98ece2676fc653a889ae63fb8db068621c88
Alisher778 <internet-docs@mail.ru>
2016-11-15 12:00:43 -0500

    prepare for submitting

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-
+var path = require('path');
 
 app.locals.moment = require('moment');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
 app.set('view engine', 'ejs');
 // app.set('views', __dirname + '/template'); // IF you want to change views into another folder
-app.use('/static', express.static(__dirname+'public'));
+app.use('/public', express.static(path.join(__dirname + '/public')));
 
 
 //Root path
@@ -24,25 +24,23 @@ app.get('/', function(req, res){
 
 // This is a dummy data
 var posts=[{
-      "id":"",
-      "title": "First officail title",
-      "message": "HI. it is so fun to creat a post using express_js",
+      "title": "6 things you should know about Node.js",
+      "message": "JavaScript is eating the world, with new tools and enhancements arriving at a breakneck pace. With Node.js, an open source runtime system invented in 2009 by Ryan Dahl, that reach has extended to the server side.Node.js has become wildly popular, with coders everywhere using it to create APIs and build a new matrix of interoperability across the Internet. Joyent has been the chief sponsor of Node.js from the beginning. In this week's New Tech Forum, Ben Wen, vice president of product marketing at Joyent, outlines six things you should know about the phenomenon shaking up backend development. -- Paul VeneziaNode.js is a runtime system for creating (mostly) server-side applications. It's best known as a popular means for JavaScript coders to build real-time Web APIs.",
       "timestamp": "Sun Nov 13 2016 20:34:17 GMT-0500 (EST)"
 
     },
     {
-      "id":"",
-      "title": "About Node JS",
-      "message": "I am using NOde JS and very fasinatng to see how powerful is!",
+      "title": "Interesting question why JavaScript is everywhere",
+      "message": "JavaScript is a quirky, object-oriented, C-like language. It's the only choice for developing applications in the browser, with a new framework introduced every week to woo developers. And with Node.js, JavaScript has spilled over to the server. Competing implementation teams have driven JavaScript interpreters forward, so that Google's V8 engine is respectably fast -- fast enough to reside at the core of Node.js.JavaScript also has the internal capability to handle the event loop mechanism in a straightforward way. Other languages have this capability, which are used by their own evented systems. Python has Twisted and Ruby has EventMachine. But because of history, both of those event-loop systems come freighted with relatively easy ways to make a particular kind of performance mistake, while JavaScript remains relatively free of this peril.",
       "timestamp": "Sun Nov 13 2016 20:34:17 GMT-0500 (EST)"
     }]
 
 
 app.get('/posts/:title', function(req, res){
-  
-  var title=
-    req.query.title;
-  res.render('queryPage', {post:posts});
+  var title = req.params.title;
+  var body = posts[title];
+  console.log(title);
+  res.render('queryPage', {posts});
 })
 
 
@@ -62,11 +60,11 @@ app.get('/create', function(req, res){
 
 app.post('/sent', function(req, res){
   var newPosts = {
-    id:"",
     title:req.body.title,
     message:req.body.message,
     timestamp: new Date(), 
   };
+  console.log(newPosts);
   posts.push(newPosts);
   res.redirect('/blog');
 });
